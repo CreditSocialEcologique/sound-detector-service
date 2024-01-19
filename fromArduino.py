8d93660df89137914ebe5f5a581fd6f222e9fd89
Steenall <12674641+Steenall@users.noreply.github.com>
2024-01-19 13:46:01 +0100

    Update fromArduino.py

--- fromArduino.py
+++ fromArduino.py
@@ -4,6 +4,7 @@ import serial
 import asyncio
 import aiohttp
 
+# Méthode permettant de faire une requete sur le web asynchrone. L'échec de la requete ne génère pas d'exception.
 async def send_request(url, data):
     try:
         async with aiohttp.ClientSession() as session:
@@ -15,22 +16,21 @@ async def send_request(url, data):
 
 
 def main():
-    # Replace 'COM3' with your Arduino port
-    arduino_port = '/dev/ttyACM0'
-    baud_rate = 9600
-    base_url = "http://intensif05.ecole.ensicaen.fr:8080/"
-    route = "api/sound/reduceScore"
-    id_device = "1"
+    arduino_port = '/dev/ttyACM0' # Le port peut changer. Sur Windows, cela sera probablement COM3
+    baud_rate = 9600 # Le nombre de symbole par seconde (un arduino est par défaut configuré sur 9600)
+    base_url = "http://intensif05.ecole.ensicaen.fr:8080/" # L'url de l'api
+    route = "api/sound/reduceScore" # La route a appelé
+    id_device = "1" # correspond à l'id de l'utilisateur
 
     try:
-        arduino = serial.Serial(arduino_port, baud_rate, timeout=1)
+        arduino = serial.Serial(arduino_port, baud_rate, timeout=1) # Se connecte à l'arduino
         print(f"Connected to {arduino_port} at {baud_rate} baud")
 
         while True:
-            data = arduino.readline().decode('utf-8').strip()
-            if "1" in data:
+            data = arduino.readline().decode('utf-8').strip() # Lit ce que l'arduino envie
+            if "1" in data: # Quand l'arduino envoie 1, cela veut dire qu'il y a de la pollution sonore
                 print("LOUD SOUND DETECTED!")
-                asyncio.run(send_request(base_url + route, {"id_user": id_device}))
+                asyncio.run(send_request(base_url + route, {"id_user": id_device})) # Crée une tache asynchrone qui va lancer la requete web
 
     except serial.SerialException as e:
         print(f"Error: {e}")
